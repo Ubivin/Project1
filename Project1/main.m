a93409b41f09f485c2b7223ee0bcc9bbb0425789
Ubivin <ubivin@gmail.com>
2014-10-19 16:39:15 -0700

    Guess a number complete

--- Project1/main.m
+++ Project1/main.m
@@ -10,9 +10,32 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int num = 400;
-        // insert code here...
-        NSLog(@"num is %i", num);
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
+        answer = arc4random() % 100 +1;
+        while (guess !=answer){
+            turn++;
+            NSLog(@"Guess #%i: Enter a number between 1-100", turn);
+            scanf("%i", &guess);
+            if( guess > answer){
+                NSLog(@"Lower!");
+                
+            }
+            else if ( guess < answer){
+                NSLog(@"Higher!");
+            }
+            else{
+                NSLog(@"Correct!");
+            
+                
+ 
+            }
+            
+        }
+        NSLog(@"It took you %i tries", turn);
+     
     }
-    return 0;
+return 0;
 }
+
